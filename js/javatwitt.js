ad0998068c64fe9cb9808ac31b9453a3f9cd0791
Nathan Archibald <narchie2013@gmail.com>
2014-04-08 10:12:55 -0600

    main steps but #5

--- js/javatwitt.js
+++ js/javatwitt.js
@@ -1,16 +1,13 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-$('.tweet-compose').focus(function(){
+$('#tweet-content>.tweet-compose').focus(function(){
 	$("#tweet-controls").show();
 })
 
 
 $('.tweet-compose').focus(function () {
     $(this).animate({ height: "4em" }, 500);
-
-
- 
 
 
 maxCharacters = 140;
@@ -20,17 +17,37 @@ $('.tweet-compose').bind('keyup keydown', function() {
     var characters = $(this).val().length;
 
     if (characters > maxCharacters) {
-        count.addClass('.over');
+        count.addClass('over');
     } else {
-        count.removeClass('.over');
+        count.removeClass('over');
     }
 
     count.text(maxCharacters - characters);
+$(function(){
+     $('.button').click(function(){
+         if ($(this).val() > '0') {
+               $('.enableOnInput').prop('disabled', true);
+          } else {
+              $('.enableOnInput').prop('disabled', false);
+          }
+     $
+     });
+});
+});
+$(".reply").click(function(){
+  $(".stats").css("display", "block");
+  $(".stats").show();
+ 
 });
 
-   	});
 
 
-})
+
+});
+
+});
+
+
+
 
 
